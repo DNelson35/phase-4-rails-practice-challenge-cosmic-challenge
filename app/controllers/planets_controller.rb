bdb3ca6e59643c1c4d0b7c69749f847f53c5af2c
Damien Nelson <dhnisaman@gmail.com>
2023-05-24 10:11:12 -0400

    commit tree 2b7fead5c37b5f68e56ad9a571799b37107e206c

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -7,4 +7,33 @@ class PlanetsController < ApplicationController
 
     render json: planets
   end
+
+  def show 
+    planet = Planet.find(params[:id])
+    render json: planet
+  end
+
+  def create 
+    planet = Planet.create!(planet_params)
+    render json: planet
+  end
+
+  def update
+    planet = Planet.find(parmas[:id])
+    planet.update!(planet_params)
+    render json: planet
+  end
+
+  def destroy
+    planet = Planet.find(params[:id])
+    planet.destroy
+    headers :no_content
+  end
+
+  private
+
+  def planet_params
+    params.permit(:id,:name,:distance_from_earth,:nearest_star,:image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   resources :missions, only: :create
-  resources :planets, only: :index
+  resources :planets
   resources :scientists
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
